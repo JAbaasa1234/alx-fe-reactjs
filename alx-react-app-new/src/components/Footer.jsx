45c902da8291e254e16d96601c01ba1ad21d5bf9
Jerry Abaasa <jerryabaasa@gmail.com>
2024-11-08 23:38:01 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer() {
+    return (
+        <footer style={{
+            backgroundColor: '#333',
+            color: '#f9f9f9',
+            padding: '10px',
+            fontFamily: 'Arial, sans-serif',
+            textAlign: 'center',
+            position: 'relative',
+            width: '100%',
+            boxShadow: '0px -2px 5px rgba(0, 0, 0, 0.2)',
+            top: '20px'
+        }}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+function MainContent() {
+    return (
+       <main style={{
+        backgroundColor: '#f0f8ff',
+        padding: '20px',
+        fontFamily: 'Arial, sans-serif',
+        textAlign: 'center',
+        color: '#333',
+        borderRadius: '8px',
+        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)'
+       }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+       </main> 
+    );
+}
+
+export default MainContent;
