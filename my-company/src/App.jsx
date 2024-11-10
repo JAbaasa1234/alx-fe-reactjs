d30bc5ce55f287b07223ae8bac502e33be801a4f
Jerry Abaasa <jerryabaasa@gmail.com>
2024-11-10 23:08:14 +0300

    Build a Simple Company Website with React

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -7,18 +7,18 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+  return(
+      <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/contact' element={<Contact />} />
+          </Routes>
+          <Footer />
+      </Router>
+  );
 }
 
 export default App;
