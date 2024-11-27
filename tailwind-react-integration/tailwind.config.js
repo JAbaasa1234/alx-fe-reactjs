ec524f6679b5f882ba2805dcf03cfd36c9e6149d
Jerry Abaasa <jerryabaasa@gmail.com>
2024-11-27 14:17:04 +0300

    Setting Up Tailwind CSS in a New React Project

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
-}
+};
 
